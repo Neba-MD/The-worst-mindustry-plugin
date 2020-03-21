0aa6168fed9aa96dd8ce3830cab4c2a9ed8a512c
jakubDoka <mlokogrgel@gmail.com>
2020-03-21 21:58:11 +0100

    n

--- src/main/java/example/Build_request.java
+++ src/main/java/example/Build_request.java
@@ -19,14 +19,10 @@ public class Build_request{
     public Build_request(String unitName,int time,UnitFactory factory){
         this.unitName=unitName;
         this.time=time;
-
         this.factory=factory;
         start_countdown();
     }
     public boolean info(Player player){
-        if(interrupted){
-            return false;
-        }
         if (building){
             player.sendMessage("Factory is currently building [orange]" + unitName + "[white].It will be finished in " +
                     time / 60 + "min" + time % 60 + "sec.");
@@ -46,11 +42,6 @@ public class Build_request{
         };
         Timer.schedule(countdown,0,1,time-1);
         Timer.schedule(() -> {
-            if (interrupted) {
-                factory.requests.remove(this);
-                return;
-            }
-
             building = false;
             Call.sendMessage("[green]" + unitName + " is finished and waiting in a hangar.You can use factory egan.");
             factory.unitStats.get(unitName)[UnitFactory.unitCount]+=1;
